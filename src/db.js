1b41258812d377d0fd5dbcc7cfa82db23a68037a
gholive84 <gholive@gmail.com>
2026-04-04 11:26:35 -0300

    fix: support Unix socket connection for MySQL (Hostinger)

--- src/db.js
+++ src/db.js
@@ -1,15 +1,22 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
+};
+
+if (process.env.DB_SOCKET) {
+  config.socketPath = process.env.DB_SOCKET;
+} else {
+  config.host = process.env.DB_HOST;
+  config.port = process.env.DB_PORT;
+}
+
+const pool = mysql.createPool(config);
 
 module.exports = pool;
